d875499d94597c5c92c69cf1a2a2ea85d4836f7c
Yash-S-Shetye <87604378+Yash-S-Shetye@users.noreply.github.com>
2021-07-21 14:26:03 +0530

    Update Contact_Less_Sanitizing_Machine.ino

--- Contact_Less_Sanitizing_Machine.ino
+++ Contact_Less_Sanitizing_Machine.ino
@@ -1,12 +1,3 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
-*/
-
 #include <Servo.h>
 
 const int trigPin = 8;
@@ -15,13 +6,12 @@ const int echoPin = 11;
 long duration;
 int distance;
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+Servo myservo; 
 
-int pos = 0;    // variable to store the servo position
+int pos = 0; 
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(9);  
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   Serial.begin(9600);
